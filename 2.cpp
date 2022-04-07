e23c9146f73dcbbc769543c7cad08e7c3f582518
Trezen Parmar <trezen7984891023@gmail.com>
2022-04-07 12:32:20 +0530

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,1 +1,54 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class charinput
+{
+    public :
+        
+        string str = "";
+        
+        void add(char str1) 
+        {
+            str = str + str1;
+        }
+        
+        string getvalue() 
+        {
+            return str;
+        }
+};      
+
+class numinput : public charinput 
+{
+    public:
+    
+    string getvalue()
+    {
+        return str;
+    }
+    
+    void add(char str2)
+    {
+        if(!(str2 >= 'a' && str2 <= 'z'))
+        {
+            charinput::add(str2);
+            // add(str2);
+        }
+    }
+};
+
+int main()
+{
+    charinput *inputc = new charinput();
+    numinput *inputn = new numinput();
+    inputn->add('1');
+    inputn->add('a');
+    inputn->add('0');
+    
+    cout << inputn->getvalue();
+
+    return 0;
+}
+
